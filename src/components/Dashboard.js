063c316b27b9cdbff46da23f9625a2dd41d3874a
paulVer777 <pawel.gitara@onet.eu>
2018-04-26 21:57:37 +0200

    initial commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,93 @@
 import React from 'react'
+import {PieChart, Pie, Tooltip} from 'recharts'
 
 
-const Dashboard = () => (<div> Dashbord </div>)
+const data = [
+    {
+        value: 70,
+        name: 'Papierosy',
+        color: 'red'
+    },
+    {
+        value: 10,
+        name: 'Słodycze'
+    },
+
+    {
+        value: 20,
+        name: 'Alkohol'
+    }
+
+];
+
+const Dashboard = () => {
+
+
+    return (
+
+        <div>
+            <PieChart width={400} height={400}>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    fill="#444444"
+                />
+                <Tooltip/>
+            </PieChart>
+
+
+            <PieChart width={400} height={400}>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    fill="#773355"
+                />
+                <Tooltip/>
+            </PieChart>
+        </div>
+    )
+};
 
 
 export default Dashboard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
