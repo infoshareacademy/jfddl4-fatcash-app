3316514a1d06ecbf977c5a32fdac3931aac9db1d
improcode <art.milkowski@gmail.com>
2018-05-08 16:42:57 +0200

    search slider

--- src/components/OperationList.js
+++ src/components/OperationList.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Slider from 'material-ui/Slider'
 
 
-const OperationList = () => (<div> List of Operations </div>)
+
+const OperationList = () => (
+    <div>
+        List of Operations
+
+    <div><Slider name="slider0" defaultValue={0.5}/></div>
+</div>
+)
+
 
 
 export default OperationList
