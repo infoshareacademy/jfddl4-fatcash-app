5d7a7968543cd53927fd17f111d6d4462c57eb6d
vizzroot <vizzroot@gmail.com>
2018-05-08 16:59:27 +0200

    changes

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -2,56 +2,58 @@ import React from 'react'
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import AddOperation from "./AddOperation";
 
 const styles = {
     block: {
         maxWidth: 250,
     },
     radioButton: {
-        marginLeft:50,
+        marginLeft: 50,
         marginBottom: 16,
     },
 };
 
 const style = {
     margin: 10,
-    marginLeft:50,
+    marginLeft: 50,
 };
 
 
 class AddCategory extends React.Component {
+    state = {
+        selectedKindOfCategory: 'exp'
+    }
+
     render() {
         return (
             <div>
-                <RadioButtonGroup name="shipSpeed" defaultSelected="not_light">
+                <RadioButtonGroup
+                    name="shipSpeed"
+                    defaultSelected="exp"
+                    onChange={(e, value) => this.setState({selectedKindOfCategory: value})}
+                >
                     <RadioButton
-                        value="light"
-                        label="icome"
+                        value="income"
+                        label="income"
                         style={styles.radioButton}
-                        onChange={()=>{alert('income')}}
-
                     />
                     <RadioButton
-                        value="not_light"
+                        value="exp"
                         label="expenses"
                         style={styles.radioButton}
-                        onChange={()=>{alert('expenses')}}
                     />
                 </RadioButtonGroup>
                 <TextField
                     hintText="Type your cattegory"
                     fullWidth={true}
-                /><br/>
-                <RaisedButton label="Add category"
-                              primary={true} style={style}
-                              fullWidth={true}
-                              onClick
-
-
                 />
-
-
+                <br/>
+                <RaisedButton
+                    label="Add category"
+                    primary={true} style={style}
+                    fullWidth={true}
+                    onClick
+                />
             </div>
         )
     };
