53b5dd4c22aa0537848d8e4270918788e1f18392
improcode <art.milkowski@gmail.com>
2018-05-10 16:29:01 +0200

    WIP

--- src/components/OperationList.js
+++ src/components/OperationList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import InputRange from 'react-input-range'
 import TextField from 'material-ui/TextField'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import MenuItem from 'material-ui/MenuItem'
 
 import 'react-input-range/lib/css/index.css'
 
@@ -9,24 +11,41 @@ class OperationList extends React.Component {
         super(props);
 
         this.state = {
-            value: {min: 2, max: 10},
+
+            value: {min: 0, max: 5000},
+            valueDrop: 1
         };
     }
+
+
+
+
+    handleChange = (event, index, valueDrop) => this.setState({valueDrop});
+
     render(){
             return (
                 <div>
                     List of Operations
                     <div>
                         <TextField
-                            hintText="Hint Text"
+                            hintText="Wyszukaj..."
+
                         />
                     </div>
 
                     <InputRange
-                        maxValue={20}
+                        maxValue={5000}
                         minValue={0}
                         value={this.state.value}
                         onChange={value => this.setState({ value })} />
+
+                    <DropDownMenu value={this.state.valueDrop} onChange={this.handleChange}>
+                        <MenuItem value={1} primaryText="Never" />
+                        <MenuItem value={2} primaryText="Every Night" />
+                        <MenuItem value={3} primaryText="Weeknights" />
+                        <MenuItem value={4} primaryText="Weekends" />
+                        <MenuItem value={5} primaryText="Weekly" />
+                    </DropDownMenu>
 
                 </div>
             )
