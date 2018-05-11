6ab16eb7635e4ebbe2febe546697f70ed2ad7822
improcode <art.milkowski@gmail.com>
2018-05-11 17:30:17 +0200

    friday commit WIP

--- src/components/OperationList.js
+++ src/components/OperationList.js
@@ -1,3 +1,6 @@
+// Jak zrobić filtry!!!! Wpisując do searcha aktualizujemy state. filtry to oddzielne funkcje, które aktualizują transaction w state
+
+
 import React from 'react'
 import InputRange from 'react-input-range'
 import TextField from 'material-ui/TextField'
@@ -12,43 +15,107 @@ class OperationList extends React.Component {
 
         this.state = {
 
-            value: {min: 0, max: 5000},
-            valueDrop: 1
+            valueRange: {min: 0, max: 5000},
+            valueDrop: 1,
+            transactions: "",
+            category: "",
+            date: "",
+            description: "",
+            income: "",
+            value: "",
+            categories: []
         };
     }
 
+    componentDidMount() {
+        this.loadTransaction()
 
+    }
+
+    // assignCategoryFromProps() {
+    //     if (this.props.match.params.param_name) {
+    //         this.setState({
+    //             category: this.props.match.params.param_name
+    //
+    //             //Paweł przekazuje mi parametr i trzeba go wsadzić w category w state
+    //         })
+    //     }
+    // }
+
+    mapObjectToArray = (obj) => (
+        Object.entries(obj || {})
+            .map(([key, value]) => (
+                typeof value === 'object' ?
+                    {...value, key}
+                    :
+                    {key, value}
+            ))
+    )
+
+    loadTransaction = () => {
+        fetch('https://fatcash-app.firebaseio.com/transactions/.json')
+            .then(r => r.json())
+            .then((data) => {
+                const transactionInArray = this.mapObjectToArray(data)
+
+                this.setState({
+                    transactions: transactionInArray,
+                    category: "",
+                    date: "",
+                    description: "",
+                    income: "",
+                    value: ""
+                })
+            })
+
+    }
+
+    // loadCategories = () => {
+    //
+    // }
 
 
     handleChange = (event, index, valueDrop) => this.setState({valueDrop});
 
-    render(){
-            return (
+    render() {
+        return (
+            <div>
+                List of Operations
                 <div>
-                    List of Operations
-                    <div>
-                        <TextField
-                            hintText="Wyszukaj..."
-
-                        />
-                    </div>
-
+                    <TextField
+                        hintText="Wyszukaj..."
+                        onChange={(e, value) => {
+                            this.setState({
+                                description: value
+                            })
+                        }}
+                    />
+                </div>
+                <div style={{margin: '100px'}}>
                     <InputRange
                         maxValue={5000}
                         minValue={0}
-                        value={this.state.value}
-                        onChange={value => this.setState({ value })} />
+                        value={this.state.valueRange}
+                        onChange={valueRange => this.setState({valueRange})}
 
-                    <DropDownMenu value={this.state.valueDrop} onChange={this.handleChange}>
-                        <MenuItem value={1} primaryText="Never" />
-                        <MenuItem value={2} primaryText="Every Night" />
-                        <MenuItem value={3} primaryText="Weeknights" />
-                        <MenuItem value={4} primaryText="Weekends" />
-                        <MenuItem value={5} primaryText="Weekly" />
-                    </DropDownMenu>
-
+                    />
                 </div>
-            )
+                <DropDownMenu value={this.state.valueDrop}
+                              onChange={this.handleChange}>
+                    <MenuItem value={1}
+                              primaryText="Never"/>
+                    <MenuItem value={2}
+                              primaryText="Every Night"/>
+                    <MenuItem value={3}
+                              primaryText="Weeknights"/>
+                    <MenuItem value={4}
+                              primaryText="Weekends"/>
+                    <MenuItem value={5}
+                              primaryText="Weekly"/>
+                </DropDownMenu>
+
+            </div>
+        )
     }
 }
 
