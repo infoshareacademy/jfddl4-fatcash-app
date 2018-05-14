ba64d65751a53c2d052edf2857cb90b12738fffb
janch11 <jandominikchoma@gmail.com>
2018-05-14 14:03:18 +0200

    delete bug with not working filter operation list without operation desc

--- src/components/AddOperation/AddOperation.js
+++ src/components/AddOperation/AddOperation.js
@@ -8,6 +8,7 @@ import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import SelectField from 'material-ui/SelectField'
 import FlatButton from 'material-ui/FlatButton'
 import Dialog from 'material-ui/Dialog';
+import Paper from 'material-ui/Paper';
 
 
 class AddOperation extends React.Component {
@@ -138,23 +139,23 @@ class AddOperation extends React.Component {
 
     saveTaskToDatabase = () => {
 
-        this.state.value.length===0 || this.state.category.length===0 ? alert("You must add value and choose category !!") :
+        this.state.value.length === 0 || this.state.category.length === 0 ? alert("You must add value and choose category !!") :
 
-        fetch('https://fatcash-app.firebaseio.com/transactions/.json',
-            {
-                method: 'POST',
-                body: JSON.stringify
-                (
-                    {
-                        category: this.state.category,
-                        date: Date.now(),
-                        description: this.state.description,
-                        income: this.state.income,
-                        value: this.state.value,
-                    }
-                )
-            }
-        ).then(this.loadTransaction)
+            fetch('https://fatcash-app.firebaseio.com/transactions/.json',
+                {
+                    method: 'POST',
+                    body: JSON.stringify
+                    (
+                        {
+                            category: this.state.category,
+                            date: Date.now(),
+                            description: this.state.description,
+                            income: this.state.income,
+                            value: this.state.value,
+                        }
+                    )
+                }
+            ).then(this.loadTransaction)
         alert('Operation sucesfully added')
     }
 
@@ -170,78 +171,80 @@ class AddOperation extends React.Component {
             ,];
 
         return (
-            <div>
+            <div style={{margin: "20px"}}>
 
 
-                <RadioButtonGroup
-                    name="shipSpeed"
-                    defaultSelected={true}
-                    onChange={this.newIncomeHandler}
-                >
-                    <RadioButton
-                        value={true}
-                        label="income"
+                    <RadioButtonGroup
+                        name="shipSpeed"
+                        defaultSelected={true}
+                        onChange={this.newIncomeHandler}
+                    >
+                        <RadioButton
+                            value={true}
+                            label="income"
 
-                    />
-                    <RadioButton
-                        value={false}
-                        label="expense"
+                        />
+                        <RadioButton
+                            value={false}
+                            label="expense"
 
-                    />
-                </RadioButtonGroup>
+                        />
+                    </RadioButtonGroup>
 
-                {this.state.income === true ?
-                    <SelectField  value={this.state.category} floatingLabelText="Choose category of your income" fullWidth={true} onChange={this.newCategoryHandler}>
-                        {
-                            this.state.categoriesInc.map((el) => (
+                    {this.state.income === true ?
+                        <SelectField value={this.state.category} floatingLabelText="Choose category of your income"
+                                     fullWidth={true} onChange={this.newCategoryHandler}>
+                            {
+                                this.state.categoriesInc.map((el) => (
+
+                                        <MenuItem value={el.name} primaryText={el.name}/>
+
+                                    )
+                                )}
+
+                        </SelectField>
+                        :
+                        <SelectField value={this.state.category} floatingLabelText="Choose category of your expence"
+                                     fullWidth={true} onChange={this.newCategoryHandler}>
+                            {this.state.categoriesExp.map((el) => (
 
                                     <MenuItem value={el.name} primaryText={el.name}/>
 
                                 )
                             )}
+                        </SelectField>
 
-                    </SelectField>
-                    :
-                    <SelectField value={this.state.category} floatingLabelText="Choose category of your expence" fullWidth={true} onChange={this.newCategoryHandler}>
-                        {this.state.categoriesExp.map((el) => (
-
-                                <MenuItem value={el.name} primaryText={el.name}/>
-
-                            )
-                        )}
-                    </SelectField>
-
-                }
+                    }
 
 
-                <Divider/>
-                {/*<TextField*/}
-                {/*value={this.state.date}*/}
-                {/*onChange={this.newDateHandler}*/}
-                {/*/>*/}
-                <TextField
-                    value={this.state.description}
-                    hintText={"Write description..."}
-                    fullWidth={true}
-                    onChange={this.newDescriptionHandler}
-                />
-                <Divider/>
+                    <Divider/>
+                    {/*<TextField*/}
+                    {/*value={this.state.date}*/}
+                    {/*onChange={this.newDateHandler}*/}
+                    {/*/>*/}
+                    <TextField
+                        value={this.state.description}
+                        hintText={"Write description..."}
+                        fullWidth={true}
+                        onChange={this.newDescriptionHandler}
+                    />
+                    <Divider/>
 
-                <TextField
-                    value={this.state.value}
-                    hintText={'Write value of your income or expence...'}
-                    fullWidth={true}
-                    type={'number'}
-                    onChange={this.newValueHandler}
-                />
-                <Divider/>
-                <RaisedButton
-                    onClick={this.saveTaskToDatabase}
-                    fullWidth={true}
-                    primary={true}
-                    label={"SAVE IT!"}
-                    disabled={this.state.value && this.state.category ? false : true}
-                />
+                    <TextField
+                        value={this.state.value}
+                        hintText={'Write value of your income or expence...'}
+                        fullWidth={true}
+                        type={'number'}
+                        onChange={this.newValueHandler}
+                    />
+                    <Divider/>
+                    <RaisedButton
+                        onClick={this.saveTaskToDatabase}
+                        fullWidth={true}
+                        primary={true}
+                        label={"SAVE IT!"}
+                        disabled={this.state.value && this.state.category ? false : true}
+                    />
 
 
                 {
