ec641f6c24203bc661d67873d28b9293e9227ae5
janch11 <jandominikchoma@gmail.com>
2018-05-14 15:41:26 +0200

    all is done

--- src/components/OperationList.js
+++ src/components/OperationList.js
@@ -138,51 +138,57 @@ class OperationList extends React.Component {
 
         return (
             <div>
-                <div style={{ border: '30px solid #f3f3f5' }}>
-                    <TextField
-                        hintText="Search..."
-                        onChange={(e, value) => {
-                            this.setState({
-                                description: value
-                            })
-                        }}
-                    />
+
+                <div style={{border: '30px solid #f3f3f5'}}>
+                    <div style={{margin: '10px 25px 10px 25px'}}>
+                        <TextField
+                            hintText="Search..."
+                            fullWidth={true}
+                            onChange={(e, value) => {
+                                this.setState({
+                                    description: value
+                                })
+                            }}
+                        />
+                    </div>
+                    <div style={{margin: '30px 25px 10px 25px'}}>
+                        <InputRange
+                            maxValue={5000}
+                            minValue={0}
+                            value={this.state.valueRange}
+                            onChange={valueRange => this.setState({valueRange})}
+                        />
+                    </div>
+
+                    <div style={{margin: '10px 25px 10px 25px'}}>
+                        Incomes:
+                        <DropDownMenu
+                            value={this.state.valueDrop}
+                            onChange={this.handleChange}
+                        >
+
+                            {this.state.categoriesInc.map((el) => (
+                                    <MenuItem value={el.name} primaryText={el.name} label={el.name}/>
+                                )
+                            )}
+                        </DropDownMenu>
+                        Expences:
+                        <DropDownMenu
+                            value={this.state.valueDrop}
+
+                            onChange={this.handleChange}
+                        >
+                            {this.state.categoriesExp.map((el) => (
+                                    <MenuItem
+                                        value={el.name}
+                                        primaryText={el.name}
+                                        label={el.name}
+                                    />
+                                )
+                            )}
+                        </DropDownMenu>
+                    </div>
                 </div>
-                <div style={{margin: '30px 25px 10px 25px'}}>
-                    <InputRange
-                        maxValue={5000}
-                        minValue={0}
-                        value={this.state.valueRange}
-                        onChange={valueRange => this.setState({valueRange})}
-                    />
-                </div>
-                Incomes:
-                <DropDownMenu
-                    value={this.state.valueDrop}
-                    onChange={this.handleChange}
-                >
-
-                    {this.state.categoriesInc.map((el) => (
-                            <MenuItem value={el.name} primaryText={el.name} label={el.name}/>
-                        )
-                    )}
-                </DropDownMenu>
-                Expences:
-                <DropDownMenu
-                    value={this.state.valueDrop}
-
-                    onChange={this.handleChange}
-                >
-                    {this.state.categoriesExp.map((el) => (
-                            <MenuItem
-                                value={el.name}
-                                primaryText={el.name}
-                                label={el.name}
-                            />
-                        )
-                    )}
-                </DropDownMenu>
-
                 <br/>
 
                 <Divider/>
