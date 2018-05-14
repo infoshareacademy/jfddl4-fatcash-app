8031c6b5bab7d3d5b7f73d407f9259bee78eb56a
vizzroot <vizzroot@gmail.com>
2018-05-14 16:46:03 +0200

    imageonsidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-import {Link} from 'react-router-dom'
-
+import {Link} from 'react-router-dom';
+import photo from '../img/photo.jpg';
+import wallet from '../img/wallet.png';
 
 const styles={
         textDecoration: 'none',
@@ -13,16 +14,18 @@ const styles={
 
 
 const SideBar = (props) => (
+
     <Drawer
         docked={false}
         open={props.isSideBarOpen}
         onRequestChange={props.onRequestSideBarChange}
-
         width={200}
+        img={{backgroundImage: `url(${photo}`}}
 
 
 
-    >
+
+    > <center><img src={wallet} alt="Fatcash Logo"/></center>
         <Link style={styles} to={'/'}><MenuItem> Dashboard </MenuItem></Link>
         <Link style={styles} to={'/operation-list'}><MenuItem> Operations list </MenuItem></Link>
         <Link style={styles} to={'/add-new-operation'}><MenuItem> Add new operation </MenuItem></Link>
@@ -30,9 +33,8 @@ const SideBar = (props) => (
         <Link style={styles} to={'/category-list'}><MenuItem> Category List </MenuItem></Link>
         <Link style={styles} to={'/filtered-category-list'}><MenuItem> Filtered Category List </MenuItem></Link>
 
-
-
     </Drawer>
+
 )
 
 export default SideBar
