076154b3ef5c3ef050823919dffa9ed7bbbdff7d
improcode <art.milkowski@gmail.com>
2018-05-17 16:11:50 +0200

    mapObjectToArray

--- src/components/OperationList/OperationList.js
+++ src/components/OperationList/OperationList.js
@@ -38,15 +38,6 @@ class OperationList extends React.Component {
         this.loadCategoriesInc()
     }
 
-    // mapObjectToArray = (obj) => (
-    //     Object.entries(obj || {})
-    //         .map(([key, value]) => (
-    //             typeof value === 'object' ?
-    //                 {...value, key}
-    //                 :
-    //                 {key, value}
-    //         ))
-    // )
 
     loadTransaction = () => {
         fetch('https://fatcash-app.firebaseio.com/transactions/.json')
