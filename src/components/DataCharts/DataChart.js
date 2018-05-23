ecf2785b2665240374d809c767a29fe865b93642
janch11 <jandominikchoma@gmail.com>
2018-05-23 16:21:40 +0200

    filter expences and inocmes working

--- src/components/DataCharts/DataChart.js
+++ src/components/DataCharts/DataChart.js
@@ -4,128 +4,138 @@ import {PieChart, Pie, Tooltip, Legend, Line, Cell} from 'recharts'
 import {RaisedButton} from 'material-ui'
 
 
-class DataChart extends React.Component{
+class DataChart extends React.Component {
 
     state = {
-        value: 0
+        timeDifference: 0
     }
+
     clickForHour = () => {
         this.setState({
-            value: 3600000,
+            timeDifference: 3600000,
         });
     };
     clickForDay = () => {
         this.setState({
-            value: 86400000,
+            timeDifference: 86400000,
         });
     };
     clickForWeek = () => {
         this.setState({
-            value: 86400000 * 7,
+            timeDifference: 86400000 * 7,
         });
     };
     clickForThirtyDays = () => {
         this.setState({
-            value: 86400000 * 30,
+            timeDifference: 86400000 * 30,
         });
     };
-    render(){
 
-    //
-    // clickForDay = () => (value === 86400000)
-    // clickForWeek = () => (value === 86400000 * 7)
-    // clickForThirtyDays = () => (value === 86400000 * 30)
+    getElementsWithDateLowerThanCurrentDate = (el, i, arr) => {
+        if (this && this.state.timeDifference === 0) {
+            return el;
+        }
 
+        const currentTimeDifference = Date.now() - this.state.timeDifference;
+        return el.date > currentTimeDifference;
+    }
 
+    render() {
 
-
-    const incomes = this.props.transactions.filter((el, i, arr) => el.income === true)
-    const expences = this.props.transactions.filter((el, i, arr) => el.income === false)
-
-
-    const expencesValues = expences.filter((el, i, arr) => el.date > this.state.value===0 ? 0 : Date.now() - this.state.value).map((el, i, arr) => el.value) // filter by 24h
-    const incomesValues = incomes.filter((el, i, arr) => el.date > this.state.value===0 ? 0 : Date.now() - this.state.value).map((el, i, arr) => el.value)
-
-    const incomesSum = incomesValues.reduce(function (previousValue, currentValue, index, array) {
-        return previousValue + currentValue;
+        //
+        // clickForDay = () => (value === 86400000)
+        // clickForWeek = () => (value === 86400000 * 7)
+        // clickForThirtyDays = () => (value === 86400000 * 30)
 
 
-    }, 0);
-
-    const expencesSum = expencesValues.reduce(function (previousValue, currentValue, index, array) {
-        return previousValue + currentValue;
+        const incomes = this.props.transactions.filter((el, i, arr) => el.income === true)
+        const expences = this.props.transactions.filter((el, i, arr) => el.income === false)
 
 
-    }, 0);
+        const expencesValues = expences.filter(this.getElementsWithDateLowerThanCurrentDate).map((el, i, arr) => el.value) // filter by 24h
+        const incomesValues = incomes.filter(this.getElementsWithDateLowerThanCurrentDate).map((el, i, arr) => el.value)
 
-    const expencesAndIncomes = [{value: incomesSum, color: 'green', name: 'Incomes'}, {
-        value: expencesSum,
-        color: 'red',
-        name: 'Expences'
-    }]
+        const incomesSum = incomesValues.reduce(function (previousValue, currentValue, index, array) {
+            return previousValue + currentValue;
 
 
-    console.log(expencesAndIncomes)
+        }, 0);
 
-    return (
-        <div>
+        const expencesSum = expencesValues.reduce(function (previousValue, currentValue, index, array) {
+            return previousValue + currentValue;
 
 
+        }, 0);
 
-            {incomes.length > 0 ?
-                <PieChart width={320} height={320}>
-                    <Pie
-                        data={expencesAndIncomes}
-                        dataKey="value"
-                        nameKey="name"
+        const expencesAndIncomes = [{value: incomesSum, color: 'green', name: 'Incomes'}, {
+            value: expencesSum,
+            color: 'red',
+            name: 'Expences'
+        }]
 
-                    >
-                        {
-                            expencesAndIncomes.map((value, index) => (
-                                <Cell key={`cell-${value.value}`} fill={value.color}/>
-                            ))
-                        }
-                    </Pie>
-                    <Tooltip/>
-                    <Legend verticalAlign="top" height={36}/>
-                    <Line name="Income" type="monotone" dataKey="value" />
-                    <Line name="Expences" type="monotone" dataKey="value" />
 
-                </PieChart>
-                : 'ładowanie!'}
+        console.log(expencesAndIncomes)
 
-            <RaisedButton
-                onClick={this.clickForHour}
-                fullWidth={true}
-                primary={true}
-                label={"Click for Hour "}
-                // disabled={value && category ? false : true}
-            />
-            <RaisedButton
-                onClick={this.clickForDay}
-                fullWidth={true}
-                primary={true}
-                label={"Click for Day "}
-                // disabled={value && category ? false : true}
-            />
-            <RaisedButton
-                onClick={this.clickForWeek}
-                fullWidth={true}
-                primary={true}
-                label={"Click for Week "}
-                // disabled={value && category ? false : true}
-            />
-            <RaisedButton
-                onClick={this.clickForThirtyDays}
-                fullWidth={true}
-                primary={true}
-                label={"Click for 30 days "}
-                // disabled={value && category ? false : true}
-            />
+        return (
+            <div>
 
-        </div>
-    )
-};}
+
+                {incomes.length > 0 ?
+                    <PieChart width={320} height={320}>
+                        <Pie
+                            data={expencesAndIncomes}
+                            dataKey="value"
+                            nameKey="name"
+
+                        >
+                            {
+                                expencesAndIncomes.map((value, index) => (
+                                    <Cell key={`cell-${value.value}`} fill={value.color}/>
+                                ))
+                            }
+                        </Pie>
+                        <Tooltip/>
+                        <Legend verticalAlign="top" height={36}/>
+                        <Line name="Income" type="monotone" dataKey="value"/>
+                        <Line name="Expences" type="monotone" dataKey="value"/>
+
+                    </PieChart>
+                    : 'ładowanie!'}
+
+                <RaisedButton
+                    onClick={this.clickForHour}
+                    fullWidth={true}
+                    primary={true}
+                    label={"Click for Hour "}
+                    // disabled={value && category ? false : true}
+                />
+                <RaisedButton
+                    onClick={this.clickForDay}
+                    fullWidth={true}
+                    primary={true}
+                    label={"Click for Day "}
+                    // disabled={value && category ? false : true}
+                />
+                <RaisedButton
+                    onClick={this.clickForWeek}
+                    fullWidth={true}
+                    primary={true}
+                    label={"Click for Week "}
+                    // disabled={value && category ? false : true}
+                />
+                <RaisedButton
+                    onClick={this.clickForThirtyDays}
+                    fullWidth={true}
+                    primary={true}
+                    label={"Click for 30 days "}
+                    // disabled={value && category ? false : true}
+                />
+
+            </div>
+        )
+    };
+}
+
 const mapStateToProps = state => ({
     categoriesInc: state.categoriesIncome.categories,
     categoriesExp: state.categoriesExp.categories,
